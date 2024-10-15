ef14df579dde840865718f49539d2655e2053fb8
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-16 00:45:50 +0100

    feat: Added in a sign out funtionality leveraging the FirebaseAuth library

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final user = FirebaseAuth.instance.currentUser;
+  HomePage({super.key});
+
+  // Sign user out method
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Logged In!"),
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
+        ],
+      ),
+      body: Center(
+        child: Text("Logged In As:${user!.email}"),
+      ),
     );
   }
 }
