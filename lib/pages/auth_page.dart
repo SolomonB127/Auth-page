59a9b3551cd280f84cade1414562dbe6a72db4e2
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-18 13:16:12 +0100

    feat: Checked whether User has an account - If user has an account the HomePage would be prompted - If user doesn't have an account the Registration pages - Signup/Login would be prompted

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,8 +1,8 @@
-import 'package:auth_page/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
+import 'registration.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -19,7 +19,7 @@ class AuthPage extends StatelessWidget {
         }
         // is user not logged in
         else {
-          return const LoginPage();
+          return const Registration();
         }
       },
     ));
