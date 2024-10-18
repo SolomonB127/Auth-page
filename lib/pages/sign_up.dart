2d6af73bf9b308e61e3fc1b6f445b6f88ddd8fa2
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-18 22:12:41 +0100

    Passed in the required test field

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,12 +1,255 @@
+import 'package:auth_page/components/buttons.dart';
+import 'package:auth_page/components/textfields.dart';
+import 'package:auth_page/components/tile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+class SignUpPage extends StatefulWidget {
+  final void Function()? onTap;
+  const SignUpPage({super.key, required this.onTap});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  // controllers
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passWordController = TextEditingController();
+
+  // showPassword method
+  bool isOpen = true;
+
+  void showPassword() {
+    setState(() {
+      isOpen = !isOpen;
+    });
+  }
+
+// Sign up method
+  void signUserUp() async {
+    // show loading circle
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(child: CircularProgressIndicator());
+        });
+
+    // try creating user
+    try {
+      // Sign in
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passWordController.text);
+      if (mounted) {
+        Navigator.pop(context);
+      }
+    } on FirebaseAuthException catch (e) {
+      // Pop the loader before showing error
+      if (mounted) {
+        Navigator.pop(context);
+        // error
+        showErrorMsg(e.code);
+      }
+    }
+  }
+
+// error msg
+  void showErrorMsg(String msg) {
+    if (mounted) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text(msg),
+            );
+          });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Register"),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 25,
+                ),
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 50,
+                ),
+
+                const SizedBox(
+                  height: 30,
+                ),
+                // welcome back msg
+                Text("Lets's create an account for you!",
+                    style: TextStyle(color: Colors.grey[700], fontSize: 16)),
+
+                const SizedBox(
+                  height: 25,
+                ),
+                // Textfield
+                // email field
+                Textfields(
+                  controller: emailController,
+                  labelText: "Email",
+                  obscureText: false,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // password field
+                Stack(
+                  alignment:
+                      Alignment.centerRight, // Align the icon to the right
+                  children: [
+                    Textfields(
+                      controller: passWordController,
+                      obscureText: isOpen,
+                      labelText: "Password",
+                    ),
+
+                    // Positioned icon inside the TextField
+                    Positioned(
+                      right: 20, // Align to the right edge
+                      child: IconButton(
+                        onPressed: showPassword,
+                        icon: Icon(
+                          isOpen ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // password field
+                Stack(
+                  alignment:
+                      Alignment.centerRight, // Align the icon to the right
+                  children: [
+                    Textfields(
+                      controller: passWordController,
+                      obscureText: isOpen,
+                      labelText: "Confirm Password",
+                    ),
+
+                    // Positioned icon inside the TextField
+                    Positioned(
+                      right: 20, // Align to the right edge
+                      child: IconButton(
+                        onPressed: showPassword,
+                        icon: Icon(
+                          isOpen ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // forgot password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 25,
+                ),
+                // Sign in button
+                Buttons(
+                  onTap: signUserUp,
+                  text: "Sign Up",
+                ),
+                // or continue ...
+                const SizedBox(
+                  height: 20,
+                ),
+
+                // google or apple sign in
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Divider(
+                        thickness: 1,
+                        color: Colors.grey[800],
+                      )),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          "or continue with",
+                          style: TextStyle(color: Colors.grey[400]),
+                        ),
+                      ),
+                      Expanded(
+                          child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[800],
+                      ))
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // google button
+                    Tile(imgPath: "assets/google.png"),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    // apple button
+                    Tile(imgPath: "assets/apple.png")
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // not a member? Register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Already have an account?"),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        "Login.",
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
