0572e872ef2a86e71e8d0d89bacd82c71385dc31
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-18 13:11:18 +0100

    feat: Implement Registration page with toggle between Login and Sign Up
    - Created a StatefulWidget for Registration.
    - Added a toggle function to switch between Login and Sign Up pages depending on whether user is Signed in or not.
    - Initialized the view to display the Login page by default.
    - Integrated LoginPage and SignUpPage components.

---
+++ lib/pages/registration.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+import 'sign_up.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  // initially, show login page
+  bool showlogin = true;
+
+  // toggle between Login and Register Page
+  void togglePages() {
+    setState(() {
+      showlogin = !showlogin;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showlogin) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return const SignUpPage();
+    }
+  }
+}
