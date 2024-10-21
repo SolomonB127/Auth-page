16dd7987070ebbba8fbb33c6f6ba477215ef9356
SolomonB127 <solomonakinsulire@gmail.com>
2024-10-21 11:30:34 +0100

    feat: add dynamic onTap functionality for enhanced interactivity

--- lib/components/tile.dart
+++ lib/components/tile.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Tile extends StatelessWidget {
+  final void Function()? onTap;
   final String imgPath;
-  const Tile({super.key, required this.imgPath});
+  const Tile({super.key, required this.imgPath, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white),
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.grey[200]),
-      child: Image.asset(
-        imgPath,
-        height: 40,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.grey[200]),
+        child: Image.asset(
+          imgPath,
+          height: 40,
+        ),
       ),
     );
   }
